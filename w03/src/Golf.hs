ba68dcbd1298435eb27950b4305041bfc81671b8
Daniel Holmes <daniel@danielholmes.org>
2017-03-01 11:37:21 +1100

    Some golf solutions

--- w03/src/Golf.hs
+++ w03/src/Golf.hs
@@ -1,13 +1,51 @@
 module Golf where
 
 import Data.List.Split
+import Data.List
 
 skips :: [a] -> [[a]]
-skips x = map (\i -> skipAmount i x) [0..((length x)-1)]
+skips x = map (skipAmount x) [1..(length x)]
 
-skipAmount :: Int -> [a] -> [a]
+skipAmount :: [a] -> Int -> [a]
+skipAmount l n = chop (\(x:xs) -> (x, drop (n-1) xs)) (drop (n-1) l)
+
+-- skipAmount n x = map last (filter (\l -> length l == n) (chunksOf n x))
 
 --skipAmount n list = case comps of
 --    (_, []) -> []
 --    (_, (x:xs)) -> x : skipAmount n xs
 --    where comps = splitAt n list
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima xs = map (\(_:b:_) -> b) (filter (\(a:b:c:_) -> b > a && b > c) (divvy 3 1 xs))
+
+-- localMaxima (a:as@(b:c:_)) = if b > a && b > c
+--                             then b : localMaxima as
+--                             else localMaxima as
+--localMaxima _ = []
+
+histogramNums :: [Integer]
+histogramNums = [0..9]
+
+histogramLen :: Int
+histogramLen = length histogramNums
+
+histogramBase :: [String]
+histogramBase = (replicate histogramLen '=') : (intercalate "" (map show histogramNums)) : []
+
+histogram :: [Integer] -> String
+histogram l = intercalate "\n" (rows ++ histogramBase)
+                where
+                    counts = histogramLengths l
+                    -- Strat - manually add a zero to list so maximum wont fail
+                    largestCount = maximum counts
+                    rows = map (\r -> map (\c -> if c >= r then '*' else ' ') counts) (reverse [1..largestCount])
+                    --newStars = map (pad ' ' longestStar) stars
+
+histogramLengths :: [Integer] -> [Int]
+histogramLengths l = map (\n -> length (filter (==n) l)) histogramNums
+
+pad :: a -> Int -> [a] -> [a]
+pad c l x
+    | (length x) >= l = x
+    | otherwise = pad c l (c : x)
--- w03/test/Spec.hs
+++ w03/test/Spec.hs
@@ -20,3 +20,39 @@ main = hspec $ do
 
         it "returns bool elements correctly" $
             skips [True,False] `shouldBe` [[True,False], [False]]
+
+    describe "localMaxima" $ do
+        it "returns Empty for Empty" $
+            localMaxima [] `shouldBe` []
+
+        it "returns correct for example 1" $
+            localMaxima [2,9,5,6,1] `shouldBe` [9,6]
+
+        it "returns correct for example 2" $
+            localMaxima [2,3,4,1,5] `shouldBe` [4]
+
+        it "returns correct for example 3" $
+            localMaxima [1..5] `shouldBe` []
+
+        it "returns correct for non strictly greater" $
+            localMaxima [2,9,5,6,6,1] `shouldBe` [9]
+
+    describe "histogram" $ do
+        it "returns Empty for Empty" $
+            histogram [] `shouldBe` "==========\n\
+                                    \0123456789"
+
+        it "returns correct for example 1" $
+            histogram [1,1,1,5] `shouldBe` " *        \n\
+                                           \ *        \n\
+                                           \ *   *    \n\
+                                           \==========\n\
+                                           \0123456789"
+
+        it "returns correct for example 2" $
+            histogram  [1,4,5,4,6,6,3,4,2,4,9] `shouldBe` "    *     \n\
+                                                          \    *     \n\
+                                                          \    * *   \n\
+                                                          \ ******  *\n\
+                                                          \==========\n\
+                                                          \0123456789"
