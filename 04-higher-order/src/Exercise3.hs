8727958a8201785b385c4e22ba9d762effc2e150
Daniel Holmes <daniel@danielholmes.org>
2017-03-03 15:05:10 +1100

    Nicer solution for arg flip

--- 04-higher-order/src/Exercise3.hs
+++ 04-higher-order/src/Exercise3.hs
@@ -8,4 +8,4 @@ map' :: (a -> b) -> [a] -> [b]
 map' f = foldr (\x y -> f x : y) []
 
 myFoldl :: (a -> b -> a) -> a -> [b] -> a
-myFoldl f base xs = foldr (\a b -> f b a) base xs
+myFoldl f base xs = foldr (flip f) base xs
